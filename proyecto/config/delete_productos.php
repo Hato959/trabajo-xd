9a64911e0aeaf94124c85be4804d7989c77b4a81
Marcos Aaron  Bedia Torres <84854728+Hato959@users.noreply.github.com>
2025-05-15 17:45:42 -0500

    Delete proyecto directory

--- proyecto/config/delete_productos.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('config.php');
-$id= $_GET['id'];
-$query= "DELETE FROM producto WHERE producto_id = $id";
-$conexion->query($query);
-header("Location: /proyecto/modulos/productos/lista_productos.php");
-?>
